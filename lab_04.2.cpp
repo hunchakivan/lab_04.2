e624ffd85906b51849947891d57d07f987ffe5f4
hunchakivan <hunchak.ivan84@gmail.com>
2023-10-11 15:59:07 +0300

    lab_04.2

---
+++ lab_04.2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 1. / abs(x + 2) + 1;
+		if (x < 1)
+			B = 7 * pow(x, 2) + x - 8;
+		else
+			if (x > 4)
+				B = sqrt(1 + abs(pow(cos(x), 3)));
+			else
+				B = 1. / (sin((x + 4) / sqrt(11)) / cos((x + 4) / sqrt(11))) + 3;
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
